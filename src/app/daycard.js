02f80655089d908d780735e4d4e5da4341a2d07c
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2024-12-11 20:20:51 +0800

    finished center circle

---
+++ src/app/daycard.js
@@ -0,0 +1,11 @@
+export function DayCard() {
+  return (
+    <div id="dayCard" className="rounded-l-[50px] w-[25vw] h-[70vh] bg-white">
+      <input
+        className="rounded-[20px] w-[567px] ml-[40px] py-[16px] "
+        type="text"
+        placeholder="Search"
+      ></input>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
+"use client";
 import Image from "next/image";
-
+import { DayCard } from "./daycard";
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div
+      id="container"
+      className=" relative w-[1600px] mx-auto  bg-[#F3F4F6] flex"
+    >
+      <div className="size-[245px] rounded-[120px]  bg-[#F3F4F6] absolute top-[478px] left-[679px]"></div>
+      <div id="left" className="w-[100%] h-[1200px]">
+        <DayCard />
+      </div>
+      <div id="right" className=" w-[100%] ">
+        <div className="bg-[#0F141E] h-[500px] rounded-bl-[80px]"></div>
+        <div className="bg-[#0F141E] h-[200px]  ml-[60px]"></div>
+        <div className="bg-[#0F141E] rounded-tl-[80px] h-[500px]"></div>
+      </div>
+    </div>
+  );
 }
